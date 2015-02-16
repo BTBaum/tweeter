f8c4d7dda4b09dc59313bdcbb1d0a3f8b2fbaf81
Brandon <btbaum11@gmail.com>
2015-02-16 13:43:40 -0500

    Added nav with matching routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   get 'home' => 'welcome/index'
 
+  get 'tweets/index' => 'tweets#index'
+
+  get 'tweets/new' => 'tweets#new'
+
   root 'welcome#index'
 
   devise_for :users
